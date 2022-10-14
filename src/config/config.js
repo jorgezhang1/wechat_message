87fc8c4e823c76b07231e72a426633d55fe5bd73
jorgezhang1 <115791802+jorgezhang1@users.noreply.github.com>
2022-10-14 13:31:47 +0200

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -36,30 +36,3 @@ const listConfig = {
 }
 
 /* 上述代码是需要自定义的配置项 */
-function verifyEmpty() {
-  var flag = ''
-  for (const key in params) {
-    if (Object.hasOwnProperty.call(params, key)) {
-      if (!params[key]) {
-        flag = key
-        break
-      }
-    }
-  }
-  return flag
-}
-
-// 校验为空
-if (verifyEmpty()) {
-  console.error(
-    '警告：请完善 “/src/config/config.js中的配置项: ' +
-      verifyEmpty() +
-      '”的内容,再执行代码！'
-  )
-  process.exit(0)
-}
-
-module.exports = {
-  params,
-  listConfig,
-}
